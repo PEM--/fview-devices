da8edf693f8cd713fb5e10b5ea95a0c018db820a
PEM-- <pemarchandet@gmail.com>
2014-11-16 07:53:27 +0100

    Example

---
+++ example/client/css.coffee
@@ -0,0 +1,8 @@
+css = new CSSC
+css.add '.gray-bg',
+  backgroundColor: CSSC.gray
+css.add '.desktop-svg-empty',
+  stroke: CSSC.red
+  fill: CSSC.yellow
+css.add '.desktop-svg-filled',
+  fill: CSSC.yellow
--- package.js
+++ package.js
@@ -1,17 +1,24 @@
 Package.describe({
   name: 'pierreeric:fview-devices',
-  summary: ' /* Fill me in! */ ',
-  version: '1.0.0',
-  git: ' /* Fill me in! */ '
+  summary: 'Display device in a surface for Meteor\'s famous-views.',
+  version: '0.1.0',
+  git: 'https://github.com/PEM--/fview-devices'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
-  api.addFiles('pierreeric:fview-devices.js');
-});
+  api.use('mjn:famous@0.3.1_2', 'client', { weak: true });
+  api.use('raix:famono@0.9.16', { weak: true });
+  api.use([
+    'templating@1.0.5',
+    'mquandalle:jade@0.2.9',
+    'gadicohen:famous-views@0.1.25',
+    'pierreeric:cssc@1.0.3',
+    'pierreeric:cssc-normalize@1.0.1',
+    'pierreeric:cssc-famous@1.0.1',
+    'pierreeric:cssc-colors@1.0.3'
+  ], 'client');
 
-Package.onTest(function(api) {
-  api.use('tinytest');
-  api.use('pierreeric:fview-devices');
-  api.addFiles('pierreeric:fview-devices-tests.js');
+  api.addFiles('desktop.jade', 'client');
+
 });
